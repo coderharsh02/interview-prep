626ec34b72d1b0426800929a6e0d80b385280fe1
Harsh Shah <harshshah.coder@gmail.com>
2026-02-01 15:17:05 +0530

    1 feb

--- AZ-204/AZ-204 - Examples/AZ - 101 - Virtual Machine/az101virtualmachine/obj/Debug/net7.0/az101virtualmachine.AssemblyInfo.cs
+++ AZ-204/AZ-204 - Examples/AZ - 101 - Virtual Machine/az101virtualmachine/obj/Debug/net7.0/az101virtualmachine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("az101virtualmachine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f066665efa61f2043706dc81a0a8a59d527ecc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a63a11a2c5db65ad3b17d10ae5cde93c23e91f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("az101virtualmachine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("az101virtualmachine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
